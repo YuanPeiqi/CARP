f077dc9453da744a48415b108b82dd0cffe22a65
12032465 <12032465@mail.sustech.edu.cn>
2021-11-28 21:18:56 +0800

    Initial commit

---
+++ CARP_solver.py
@@ -0,0 +1,62 @@
+import numpy as np
+import sys
+# distance[u][v] indicates the minimum distance from u to v
+distance = []
+# weight[(u,v)] indicates the cost of edge (u,v)
+weight = {}
+# demand[(u,v)] indicates the demand of task (u,v)
+demand = {}
+
+
+def Floyd(size):
+    for k in range(1, size):
+        for i in range(1, size):
+            for j in range(1, size):
+                if distance[i][k] + distance[k][j] < distance[i][j]:
+                    distance[i][j] = distance[i][k] + distance[k][j]
+
+
+def path_scanning():
+    capacity = information['CAPACITY']
+    uncompleted = [task for task in demand.keys()]
+    while uncompleted:
+        route = []
+        load, travel = 0, 0
+        src = information['DEPOT']
+        dist = 0
+        while uncompleted and dist != float('Inf'):
+            dist = float('Inf')
+            for task in uncompleted:
+                if demand[task] + load <= capacity:
+                    if distance[src][task[0]] < dist:
+                        print()
+
+if __name__ == '__main__':
+    # Receive the arguments and read the file
+    file_path = sys.argv[1]
+    termination = sys.argv[3]
+    seed = sys.argv[5]
+    instance_file = open(file_path, mode='r', newline='')
+    content = instance_file.readlines()
+    instance_file.close()
+
+    # Store the information of graph given by the file
+    information = {content[i].replace('\n','').split(' : ')[0]: int(content[i].replace('\n','').split(' : ')[1]) for i in range(1, 8)}
+
+    # Get the graph data
+    graph_size = information['VERTICES'] + 1
+    distance = [[float('Inf') if i != j else 0 for j in range(graph_size)] for i in range(graph_size)]
+    for i in range(9,len(content) - 1):
+        u, v, c, d = list(map(int, content[i].replace('\n','').split()))
+        if d:
+            demand[(u,v)] = d
+            demand[(v,u)] = d
+        weight[(u,v)] = c
+        weight[(v,u)] = c
+        distance[u][v] = c
+        distance[v][u] = c
+
+    print(information)
+    # Run Floyd's algorithm to find minimum distances of each vertices pair
+    Floyd(graph_size)
+
---
+++ test.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = float('inf')
+    print(n == float('inf'))
+    n = 1
+    print(n)
